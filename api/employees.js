0429ea8a5b7ca03fb41817e1555cce4a0bc0dc3e
Bryce <bryce.bolin@yahoo.com>
2026-08-03 21:53:18 -0500

    solution added

--- api/employees.js
+++ api/employees.js
@@ -1,5 +1,57 @@
 import express from "express";
 const router = express.Router();
 export default router;
+import {
+  createEmployee,
+  getEmployees,
+  getEmployee,
+  deleteEmployee,
+  updateEmployee,
+} from "#db/queries/employees";
 
-// TODO: this file!
+router.get("/", async (req, res) => {
+  const employees = await getEmployees();
+  res.send(employees);
+});
+
+router.get("/:id", async (req, res) => {
+  const employee = await getEmployee(req.params.id);
+  if (!employee) {
+    return res.status(404).send();
+  }
+  res.send(employee);
+});
+
+router.post("/", async (req, res) => {
+  const { name, birthday, salary } = req.body || {};
+  if (!name || !birthday || !salary) {
+    return res.status(400).send();
+  }
+  const employee = await createEmployee({ name, birthday, salary });
+  res.status(201).send(employee);
+});
+
+router.delete("/:id", async (req, res) => {
+  const employee = await deleteEmployee(req.params.id);
+  if (!employee) {
+    return res.status(404).send();
+  }
+  res.status(204).send();
+});
+
+router.put("/:id", async (req, res) => {
+  const { name, birthday, salary } = req.body || {};
+  if (!name || !birthday || !salary) {
+    return res.status(400).send();
+  }
+  const employee = await updateEmployee({
+    id: req.params.id,
+    name,
+    birthday,
+    salary,
+  });
+  if (!employee) {
+    return res.status(404).send();
+  }
+  res.status(200).send(employee);
+});
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,5 @@
 import db from "#db/client";
+import { createEmployee } from "#db/queries/employees";
 
 await db.connect();
 await seedEmployees();
@@ -7,4 +8,9 @@ console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
   // TODO
+  const employees = [];
+  for (let i = 1; i <= 10; i++) {
+    const employees = await createEmployee("Employee" + i, "1990/6/27", 50000);
+  }
+  return employees;
 }
